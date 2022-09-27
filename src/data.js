d342e270a108941e2563b76f5c62f18b12449212
ORUS <salagonfiza4@gmail.com>
2022-09-28 00:00:13 +0300

    Initial commit

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://images.unsplash.com/photo-1472289065668-ce650ac443d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "SUMMER SALE",
+        desc: "DON'T COMPROISE ON STYLE!",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://images.unsplash.com/photo-1510070112810-d4e9a46d9e91?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+        title: "WINTER SALE",
+        desc: "DON'T COMPROISE ON STYLE!",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://images.unsplash.com/photo-1488190211105-8b0e65b80b4e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+        title: "POPULAR SALE",
+        desc: "DON'T COMPROISE ON STYLE!",
+        bg: "fbf0f4",
+    },
+]
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://images.pexels.com/photos/1085965/pexels-photo-1085965.jpeg",
+        title: "MORNING COFFEE",
+    },
+    {
+        id: 2,
+        img: "https://images.pexels.com/photos/162947/coffee-glass-beverage-coffee-mug-162947.jpeg",
+        title: "DINNER COFFEE",
+    },
+    {
+        id: 3,
+        img: "https://images.pexels.com/photos/1568639/pexels-photo-1568639.jpeg",
+        title: "COFFEE FOR WORK",
+    },
+]
+
+export const popularProducts = [
+    {
+        id:1,
+        img:"https://masterica.com.ua/upload/iblock/217/karandash-tsvetnoy-highlighter-3411-tsvet-zhyeltyy-koh-i-noor.jpg",
+    },
+    {
+        id:2,
+        img:"https://masterica.com.ua/upload/iblock/0c2/karandash-tsvetnoy-highlighter-3411-tsvet-rozovyy-koh-i-noor.jpg",
+    },
+    {
+        id:3,
+        img:"https://images.prom.ua/3824721550_w640_h640_tsvetnoj-karandash-faber-castell.jpg",
+    },
+    {
+        id:4,
+        img:"https://images.prom.ua/3824724137_w640_h640_tsvetnoj-karandash-faber-castell.jpg",
+    },
+    {
+        id:5,
+        img:"https://images.prom.ua/3824721579_w640_h640_tsvetnoj-karandash-faber-castell.jpg",
+    },
+    {
+        id:6,
+        img:"https://images.prom.ua/3824721560_w640_h640_tsvetnoj-karandash-faber-castell.jpg",
+    },
+    {
+        id:7,
+        img:"https://images.prom.ua/3824726886_w640_h640_tsvetnoj-karandash-faber-castell.jpg",
+    },
+    {
+        id:8,
+        img:"https://images.prom.ua/3824726868_w640_h640_tsvetnoj-karandash-faber-castell.jpg",
+    }
+]
